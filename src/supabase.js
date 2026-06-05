af453d58b19065aac48929773deedf1cfc54289d
Victor <fotografiavictorsilva@gmail.com>
2026-06-05 09:12:59 -0300

    fix clave Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://dxdpicrfjkdmliyyouwo.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_DIGIR2ZOZrdutHtQx67cyQ_1x3pVrrh'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBpY3JmamtkbWxpeXlvdXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MzUxNjAsImV4cCI6MjA5NjIxMTE2MH0.XBxikoHXv9PHPheFRa0-w2oOt5gh6Di2IxBRyA4Ki9c'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
